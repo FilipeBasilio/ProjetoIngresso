ba1f1991c21c83046539e6f4795e968075d598ce
FilipeBasilio <filipebasilio1705@gmail.com>
2025-06-02 17:35:20 -0300

    Create Ingresso.java

---
+++ Ingresso.java
@@ -0,0 +1,17 @@
+Ingresso.java
+java
+Copiar
+Editar
+public class Ingresso {
+    protected int codigo;
+    protected double valor;
+
+    public Ingresso(int codigo, double valor) {
+        this.codigo = codigo;
+        this.valor = valor;
+    }
+
+    public void imprimeValor() {
+        System.out.printf("Código: %d | Valor: R$ %.2f%n", codigo, valor);
+    }
+}
